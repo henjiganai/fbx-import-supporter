0b2129c6cab41b5d432fc02232c4fc0a2b7ae199
henjiganai <47113387+henjiganai@users.noreply.github.com>
2021-08-05 17:34:32 +0900

    fix: アバターアップロード時にエラーが出ていた箇所を修正

--- Script/FBXImportSupporter.cs
+++ Script/FBXImportSupporter.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-
-using UnityEditor.Experimental.AssetImporters;
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FBXImportSupporter : MonoBehaviour
 {
